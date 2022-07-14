67315fe0c924c618a9907b5a475dd4c61068a666
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 15:06:58 +0500

    change all previous Header properties to object

--- src/index.js
+++ src/index.js
@@ -1,18 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Skills Component
-const Skills = (props) => {
-  // modifying the skills array
-  const skillList = props.skills.map((skill) => <li>{skill}</li>);
-  return <ul>{skillList}</ul>;
+const showDate = (time) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const month = months[time.getMonth()].slice(0, 3);
+  const year = time.getFullYear();
+  const date = time.getDate();
+  return ` ${month} ${date}, ${year}`;
+};
+// Header Component
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header-wrapper">
+        <h1>{props.data.welcome}</h1>
+        <h2>{props.data.title}</h2>
+        <h3>{props.data.subtitle}</h3>
+        <p>
+          {props.data.author.firstName} {props.data.author.lastName}
+        </p>
+        <small>{showDate(props.data.date)}</small>
+      </div>
+    </header>
+  );
 };
 
-const App = () => (
-  <div className="app">
-    <Skills skills={["HTML", "CSS", "JavaScript"]} />
-  </div>
-);
+// The App, or the parent or the container component
+// Functional Component
+const App = () => {
+  const data = {
+    welcome: "Welcome to 30 Days Of React",
+    title: "Getting Started React",
+    subtitle: "JavaScript Library",
+    author: {
+      firstName: "Asabeneh",
+      lastName: "Yetayeh",
+    },
+    date: new Date(), // date needs to be formatted to a human readable format
+  };
 
+  return (
+    <div className="app">
+      <Header data={data} />
+    </div>
+  );
+};
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
