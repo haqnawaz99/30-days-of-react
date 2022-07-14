1897606dc8e712bd81efff92c7a9097b129264f1
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 13:41:26 +0500

    Styles added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom";
 
+const headerStyles = {
+  backgroundColor: "#61DBFB",
+  fontFamily: "Helvetica Neue",
+  padding: 25,
+  lineHeight: 1.5,
+};
+
 // JSX element, header
 const header = (
-  <header>
-    <h1>Welcome to 30 Days Of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>Asabeneh Yetayeh</p>
-    <small>Oct 2, 2020</small>
+  <header style={headerStyles}>
+    <div className="header-wrapper">
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 2, 2020</small>
+    </div>
   </header>
 );
 
 // JSX element, main
+const mainStyles = {
+  backgroundColor: "#F3F0F5",
+};
 const main = (
-  <main>
+  <main style={mainStyles}>
     <p>Prerequisite to get started react.js:</p>
     <ul>
       <li>HTML</li>
@@ -24,16 +37,19 @@ const main = (
   </main>
 );
 
+const footerStyles = {
+  backgroundColor: "#61DBFB",
+};
 // JSX element, footer
 const footer = (
-  <footer>
+  <footer style={footerStyles}>
     <p>Copyright 2020</p>
   </footer>
 );
 
-// JSX element, app, a container or a parent
+// JSX element, app
 const app = (
-  <div>
+  <div className="app">
     {header}
     {main}
     {footer}
@@ -42,8 +58,4 @@ const app = (
 
 const rootElement = document.getElementById("root");
 // we render the JSX element using the ReactDOM package
-// ReactDOM has the render method and the render method takes two argument
 ReactDOM.render(app, rootElement);
-// or
-
-//ReactDOM.render(header, rootElement);
