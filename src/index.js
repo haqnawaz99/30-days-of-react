c10c46b0bfef64f7dff9229fea71770fc9fc6bb0
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 15:00:29 +0500

    Number Props

--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Header Component
-const Header = (props) => (
-  <header>
-    <div className="header-wrapper">
-      <h1>{props.welcome}</h1>
-      <h2>{props.title}</h2>
-      <h3>{props.subtitle}</h3>
-      <p>
-        {props.firstName} {props.lastName}
-      </p>
-      <small>{props.date}</small>
-    </div>
-  </header>
+const Age = (props) => <div>The person is {props.age} years old.</div>;
+const Weight = (props) => (
+  <p>The weight of the object on earth is {props.weight} N.</p>
 );
 
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
-  const welcome = "Welcome to 30 Days Of React";
-  const title = "Getting Started React";
-  const subtitle = "JavaScript Library";
-  const firstName = "Asabeneh";
-  const lastName = "Yetayeh";
-  const date = "Oct 4, 2020";
+  let currentYear = 2020;
+  let birthYear = 1820;
+  const age = currentYear - birthYear;
+  const gravity = 9.81;
+  const mass = 75;
 
   return (
     <div className="app">
-      <Header
-        welcome={welcome}
-        title={title}
-        subtitle={subtitle}
-        firstName={firstName}
-        lastName={lastName}
-        date={date}
-      />
+      <Age age={age} />
+      <Weight weight={gravity * mass} />
     </div>
   );
 };
