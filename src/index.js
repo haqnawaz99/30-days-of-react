9bf7ae9bbadc1ded8929b3880cfef89fed9d6f4f
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 13:40:39 +0500

    Header Main and Footer added in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const jsxElement = <h1>This is a JSX element in headig</h1>;
-const rootElement = document.getElementById("root");
+// JSX element, header
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+);
 
-ReactDOM.render(jsxElement, rootElement);
+// JSX element, main
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+);
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+);
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+// we render the JSX element using the ReactDOM package
+// ReactDOM has the render method and the render method takes two argument
+ReactDOM.render(app, rootElement);
+// or
+
+//ReactDOM.render(header, rootElement);
