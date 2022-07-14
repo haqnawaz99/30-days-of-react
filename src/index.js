32e97e4db2a694aea2d9a149612b1fb5fb9163a6
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 15:03:11 +0500

    Array props

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Status = (props) => {
-  // ternary operator to check the status of the person
-  let status = props.status ? "Old enough to drive" : "Too young for driving";
-  return <p>{status}</p>;
-};
+const Skills = (props) => <ul>{props.skills}</ul>;
 
-// The App, or the parent or the container component
-// Functional Component
-const App = () => {
-  let currentYear = 2020;
-  let birthYear = 2015;
-  const age = currentYear - birthYear; // 15 years
+const App = () => (
+  <div className="app">
+    <Skills skills={["HTML", "CSS", "JavaScript"]} />
+  </div>
+);
 
-  let status = age >= 18;
-
-  return (
-    <div className="app">
-      <Status status={status} />
-    </div>
-  );
-};
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
