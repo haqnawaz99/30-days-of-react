2a1b28e5fd403b03155e66265cceff4741201c3e
haqnawaz <haqnawaz99@yahoo.com>
2022-07-14 14:37:29 +0500

    Functional Components Added

--- src/index.js
+++ src/index.js
@@ -1,94 +1,67 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom";
-// To get the root element from the HTML document
+import asabenehImage from "./images/Musa.jpg";
 
-// JSX element, header
-const welcome = "Welcome to 30 Days Of React";
-const title = "Getting Started React";
-const subtitle = "JavaScript Library";
-const author = {
-  firstName: "Asabeneh",
-  lastName: "Yetayeh",
-};
-const date = "Oct 2, 2020";
-
-// JSX element, header
-const header = (
+// Header Component
+const Header = () => (
   <header>
     <div className="header-wrapper">
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 3, 2020</small>
     </div>
   </header>
 );
 
-const numOne = 3;
-const numTwo = 2;
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
+// User Card Component
+const UserCard = () => (
+  <div className="user-card">
+    <img src={asabenehImage} alt="asabeneh image" />
+    <h2>Asabeneh Yetayeh</h2>
+  </div>
 );
 
-const yearBorn = 1820;
-const currentYear = new Date().getFullYear();
-const age = currentYear - yearBorn;
-const personAge = (
-  <p>
-    {" "}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-);
+// TechList Component
+const TechList = () => {
+  const techs = ["HTML", "CSS", "JavaScript"];
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+  return techsFormatted;
+};
 
-// JSX element, main
-const techs = ["HTML", "CSS", "JavaScript"];
-const techsFormatted = techs.map((tech) => <li>{tech}</li>);
-
-// JSX element, main
-const main = (
+// Main Component
+const Main = () => (
   <main>
     <div className="main-wrapper">
-      <p>
-        Prerequisite to get started{" "}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
     </div>
   </main>
 );
 
-const copyRight = "Copyright 2020";
-
-// JSX element, footer
-const footer = (
+// Footer Component
+const Footer = () => (
   <footer>
     <div className="footer-wrapper">
-      <p>{copyRight}</p>
+      <p>Copyright 2020</p>
     </div>
   </footer>
 );
 
-// JSX element, app
-const app = (
+// The App, or the parent or the container component
+const App = () => (
   <div className="app">
-    {header}
-    {main}
-    {footer}
+    <Header />
+    <Main />
+    <Footer />
   </div>
 );
 
 const rootElement = document.getElementById("root");
-// we render the JSX element using the ReactDOM package
-ReactDOM.render(app, rootElement);
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement);
